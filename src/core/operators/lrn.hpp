f5d265e06f34009af328ba3f3aecfdf5cd2639b0
Manuela Schuler <Manuela.Schuler@dfki.de>
2021-12-05 06:45:21 +0100

    LRN operator   bwd pass

--- src/core/operators/lrn.hpp
+++ src/core/operators/lrn.hpp
@@ -27,6 +27,31 @@ struct LRNFwdContext {
   }
 };
 
+struct LRNBwdContext {
+  shared_ptr<memory> in_diff_mem;
+  shared_ptr<memory> src_mem;
+  shared_ptr<memory> ws_mem;
+  shared_ptr<memory> out_diff_mem;
+  shared_ptr<lrn_backward::desc> bwd_desc;
+  shared_ptr<lrn_backward::primitive_desc> bwd_pd;
+  shared_ptr<lrn_backward> lrn_bwd;
+
+  LRNBwdContext()
+      : in_diff_mem(nullptr), src_mem(nullptr), ws_mem(nullptr),
+        out_diff_mem(nullptr), bwd_desc(nullptr), bwd_pd(nullptr),
+        lrn_bwd(nullptr) {}
+
+  ~LRNBwdContext() {
+    in_diff_mem.reset();
+    src_mem.reset();
+    ws_mem.reset();
+    in_diff_mem.reset();
+    bwd_desc.reset();
+    bwd_pd.reset();
+    lrn_bwd.reset();
+  }
+};
+
 class LRN : public Operator {
 public:
   LRN(string n, vector<string> i, vector<string> o, float a, float be, float bi,
@@ -47,10 +72,15 @@ public:
                     vector<string>{"diff_" + o.at(0), i.at(0), o.at(0) + "_ws"},
                     vector<string>{"diff_" + i.at(0)});
     _fwd_context = nullptr;
+    _bwd_context = nullptr;
     init(tensors);
   }
-  ~LRN() { reset_fwd_primitives(); }
+  ~LRN() {
+    reset_fwd_primitives();
+    reset_bwd_primitives();
+  }
   void reset_fwd_primitives() { _fwd_context.reset(); }
+  void reset_bwd_primitives() { _bwd_context.reset(); }
 
   void forward(Device &dev, unordered_map<string, unique_ptr<Tensor>> &tensors,
                memory::format_tag outputTag, const int measure_time) {
@@ -112,13 +142,75 @@ public:
   }
 
   void backward(Device &dev, unordered_map<string, unique_ptr<Tensor>> &tensors,
-                memory::format_tag outputTag, const int measure_time) {}
-
+                memory::format_tag outputTag, const int measure_time) {
+    auto begin = get_time();
+    auto eng = dev.get_engine();
+    auto src_name = _b_op.input.at(1);
+    auto ws_name = _b_op.input.at(2);
+    auto out_name = _f_op.output.at(0);
+    auto in_diff_name = _b_op.input.at(0);
+    auto out_diff_name = _b_op.output.at(0);
+    auto src_md = tensors[src_name]->desc();
+    auto dst_md = tensors[out_name]->desc();
+    auto time_name = getBackwardTimeName(eng);
+    if (_bwd_context == nullptr) {
+      auto time_create = get_time();
+      _bwd_context.reset(new LRNBwdContext());
+      _bwd_context->bwd_desc.reset(new lrn_backward::desc(
+          {algo, src_md, dst_md, size, alpha, beta, bias}));
+      int reuse_fwd_pd = 0;
+      if (_fwd_context != nullptr) {
+        reuse_fwd_pd = _fwd_context->fwd_pd->get_engine() == eng ? 1 : 0;
+      }
+      auto fwd_pd =
+          reuse_fwd_pd
+              ? *_fwd_context->fwd_pd
+              : lrn_forward::primitive_desc({prop_kind::forward_training, algo,
+                                             src_md, size, alpha, beta, bias},
+                                            eng);
+      _bwd_context->bwd_pd.reset(new lrn_backward::primitive_desc(
+          *_bwd_context->bwd_desc, eng, fwd_pd));
+      _bwd_context->lrn_bwd.reset(new lrn_backward(*_bwd_context->bwd_pd));
+      // get memory
+      _bwd_context->src_mem.reset(
+          new memory(_bwd_context->bwd_pd.get()->src_desc(), eng));
+      _bwd_context->in_diff_mem.reset(
+          new memory(_bwd_context->bwd_pd.get()->src_desc(), eng));
+      _bwd_context->out_diff_mem.reset(
+          new memory(_bwd_context->bwd_pd.get()->src_desc(), eng));
+      tensors[out_diff_name]->init(_bwd_context->bwd_pd.get()->src_desc(), eng);
+      timings[time_name]["create"] = get_elapsed_ms(time_create);
+    }
+    // reorders
+    auto s = stream(eng);
+    timings[time_name][src_name] =
+        maybe_do_reorder(tensors[src_name]->get_memory(),
+                         *_bwd_context->src_mem, s, measure_time);
+    timings[time_name][in_diff_name] =
+        maybe_do_reorder(tensors[in_diff_name]->get_memory(),
+                         *_bwd_context->in_diff_mem, s, measure_time);
+    timings[time_name][ws_name] = maybe_do_reorder(
+        tensors[ws_name]->get_memory(), *_bwd_context->ws_mem, s, measure_time);
+    // execute
+    auto args = unordered_map<int, memory>(
+        {{DNNL_ARG_SRC, *_bwd_context->src_mem},
+         {DNNL_ARG_DIFF_DST, *_bwd_context->in_diff_mem},
+         {DNNL_ARG_DIFF_SRC, *_bwd_context->out_diff_mem},
+         {DNNL_ARG_WORKSPACE, *_bwd_context->ws_mem}});
+    auto time_exe = get_time();
+    _bwd_context->lrn_bwd->execute(s, args);
+    s.wait();
+    if (measure_time) {
+      timings[time_name]["exe"] = get_elapsed_ms(time_exe);
+      timings[time_name]["total"] = get_elapsed_ms(begin);
+    }
+  }
   algorithm algo;
   float alpha;
   float beta;
   float bias;
   int size;
   shared_ptr<LRNFwdContext> _fwd_context;
+  shared_ptr<LRNBwdContext> _bwd_context;
 };
 #endif
