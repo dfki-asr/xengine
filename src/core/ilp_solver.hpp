81b4ba2f0a4bdc98e4e72e3eab282259bcc0c80e
Manuela Schuler <Manuela.Schuler@dfki.de>
2021-12-09 12:07:31 +0100

    ILP Solver Refactoring   define Problem as MPS   (independent of Solver)

--- src/core/ilp_solver.hpp
+++ src/core/ilp_solver.hpp
@@ -30,6 +30,60 @@ string idx_to_string(const size_t d, const size_t i, const size_t j) {
   return "[" + idx2str(d, 3) + "," + idx2str(i) + "," + idx2str(j) + "]";
 }
 
+class Marker {
+public:
+  Marker() {
+    name = "";
+    mode = "";
+    values = vector<pair<string, float>>();
+    indices = vector<size_t>();
+  }
+  Marker(const string m, const string n, const vector<size_t> i) {
+    name = n;
+    mode = m;
+    values = vector<pair<string, float>>();
+    indices = i;
+  }
+  string name;
+  string mode;
+  vector<pair<string, float>> values;
+  vector<size_t> indices;
+};
+
+class Constraint {
+public:
+  Constraint() {
+    name = "";
+    mode = "";
+    rhs = 0.0;
+  }
+  Constraint(const string m, const string n, const float v) {
+    name = n;
+    mode = m;
+    rhs = v;
+  }
+  string name;
+  string mode;
+  float rhs;
+};
+
+class QuadObj {
+public:
+  QuadObj() {
+    var1 = "";
+    var2 = "";
+    coeff = 0;
+  }
+  QuadObj(const string v1, const string v2, const float c) {
+    var1 = v1;
+    var2 = v2;
+    coeff = c;
+  }
+  string var1;
+  string var2;
+  float coeff;
+};
+
 class ILP_Solver {
 public:
   ILP_Solver(string model_name, string mpsfile, string logfile,
@@ -38,6 +92,7 @@ public:
              matrix copy_costs, vector<float> budget, vector<float> ram,
              const int verbose = 0);
   ~ILP_Solver();
+  int defineProblemAsMPS();
   virtual int solve() = 0;
   void printResults();
   matrix get_R();
@@ -70,5 +125,23 @@ protected:
   int _verbose;
   string _mpsfile;
   string _logfile;
+
+private:
+  string _rows;
+  string _columns;
+  string _rhs;
+  map<string, Marker> _marker;
+  unordered_map<string, Constraint> _constraints;
+  vector<string> _constraints_insertOrder;
+  vector<QuadObj> _quad_obj;
+
+  void _num_hazards(const size_t t, const size_t d, const size_t i,
+                    const size_t k, const string c_ub, const string c_lb);
+
+  void add_constraint(const string mode, const float rhs);
+  void add_marker(const string mode, const string name, const string type,
+                  const float value, const vector<size_t> indices);
+  void add_row(const string name);
+  void add_column(const string name);
 };
 #endif
