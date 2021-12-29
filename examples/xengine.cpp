cd10c3596d4468f23cd4c17557548372343be7b1
Manuela Schuler <Manuela.Schuler@dfki.de>
2021-12-29 10:45:55 +0100

    Cleanup & Refactoring:   examples/xengine

--- examples/xengine.cpp
+++ examples/xengine.cpp
@@ -5,13 +5,12 @@ using namespace std;
 
 void createOrRunSchedule(Network &net, const string &images,
                          const string &labels) {
-  const string &schedulefile =
+  const string schedulefile =
       "../data/schedules/" + net.name() + "_" + net.mode() + "_schedule.txt";
   if (!checkIfFileExists(schedulefile)) {
     net.createSchedule(schedulefile, images, labels);
   } else {
-    const size_t num_iterations = 1;
-    net.runSchedule(schedulefile, images, labels, num_iterations);
+    net.runSchedule(schedulefile, images, labels, 1);
   }
 }
 
@@ -25,9 +24,9 @@ void runILPSolver(Network &net, const string &images, const string &labels) {
 }
 
 void execute_network(const string &model_name, string &images, string &labels,
-                     const string &devices, const int training,
-                     const string output_dir) {
-  auto model = "../data/models/" + model_name + ".onnx";
+                     const string &devices, const int &training,
+                     const string &output_dir) {
+  const string model = "../data/models/" + model_name + ".onnx";
   const int verbose = 1;
   Network net =
       Network(model_name, model, devices, training, output_dir, verbose);
@@ -42,58 +41,53 @@ void execute_network(const string &model_name, string &images, string &labels,
   runILPSolver(net, images, labels);
 }
 
-void lenet(const int batchsize, const int training, const string devices,
-           const string output_dir) {
-  string model_name, images, labels;
-  model_name = "lenet_bs" + to_string(batchsize);
-  if (training) {
-    images = "../data/datasets/mnist_train/train-images-idx3-ubyte";
-    labels = "../data/datasets/mnist_train/train-labels-idx1-ubyte";
-  } else {
-    images = "../data/datasets/mnist_test/t10k-images-idx3-ubyte";
-    labels = "../data/datasets/mnist_test/t10k-labels-idx1-ubyte";
-  }
+void lenet(const int &batchsize, const int &training, const string &devices,
+           const string &output_dir) {
+  const string model_name = "lenet_bs" + to_string(batchsize);
+  string images = training
+                      ? "../data/datasets/mnist_train/train-images-idx3-ubyte"
+                      : "../data/datasets/mnist_test/t10k-images-idx3-ubyte";
+  string labels = training
+                      ? "../data/datasets/mnist_train/train-labels-idx1-ubyte"
+                      : "../data/datasets/mnist_test/t10k-labels-idx1-ubyte";
   execute_network(model_name, images, labels, devices, training, output_dir);
 }
 
-void vgg(const string version, const int batchsize, const int training,
-         const string devices, const string output_dir) {
-  string model_name, images, labels;
-  model_name = "vgg" + version + "-7_bs" + to_string(batchsize);
-  images = "../data/datasets/imagenet_test/images-idx4-ubyte";
-  labels = "../data/datasets/imagenet_test/labels-idx1-short";
+void vgg(const string &version, const int &batchsize, const int &training,
+         const string &devices, const string &output_dir) {
+  const string model_name = "vgg" + version + "-7_bs" + to_string(batchsize);
+  string images = "../data/datasets/imagenet_test/images-idx4-ubyte";
+  string labels = "../data/datasets/imagenet_test/labels-idx1-short";
   execute_network(model_name, images, labels, devices, training, output_dir);
 }
 
-void resnet(const string version, const int batchsize, const int training,
-            const string devices, const string output_dir) {
-  string model_name, images, labels;
-  model_name = "resnet" + version + "-v1-7_bs" + to_string(batchsize);
-  images = "../data/datasets/imagenet_test/images-idx4-ubyte";
-  labels = "../data/datasets/imagenet_test/labels-idx1-short";
+void resnet(const string &version, const int &batchsize, const int &training,
+            const string &devices, const string &output_dir) {
+  const string model_name =
+      "resnet" + version + "-v1-7_bs" + to_string(batchsize);
+  string images = "../data/datasets/imagenet_test/images-idx4-ubyte";
+  string labels = "../data/datasets/imagenet_test/labels-idx1-short";
   execute_network(model_name, images, labels, devices, training, output_dir);
 }
 
-void googlenet(const int batchsize, const int training, const string devices,
-               const string output_dir) {
-  string model_name, images, labels;
-  model_name = "googlenet-7_bs" + to_string(batchsize);
-  images = "../data/datasets/imagenet_test/images-idx4-ubyte";
-  labels = "../data/datasets/imagenet_test/labels-idx1-short";
+void googlenet(const int &batchsize, const int &training, const string &devices,
+               const string &output_dir) {
+  const string model_name = "googlenet-7_bs" + to_string(batchsize);
+  string images = "../data/datasets/imagenet_test/images-idx4-ubyte";
+  string labels = "../data/datasets/imagenet_test/labels-idx1-short";
   execute_network(model_name, images, labels, devices, training, output_dir);
 }
 
-void unet(const int batchsize, const int training, const string devices,
-          const string output_dir) {
-  string model_name, images, labels;
-  model_name = "unet";
-  images = "";
-  labels = "";
+void unet(const int &batchsize, const int &training, const string &devices,
+          const string &output_dir) {
+  const string model_name = "unet";
+  string images = "";
+  string labels = "";
   execute_network(model_name, images, labels, devices, training, output_dir);
 }
 
-int run(const string name, const int batchsize, const int training,
-        const string devices, const string output_dir) {
+int run(const string &name, const int &batchsize, const int &training,
+        const string &devices, const string &output_dir) {
   if (name.compare("lenet") == 0) {
     lenet(batchsize, training, devices, output_dir);
   } else if (name.compare("vgg16") == 0) {
@@ -112,28 +106,27 @@ int run(const string name, const int batchsize, const int training,
     unet(batchsize, training, devices, output_dir);
   } else {
     throw runtime_error("Unknown NETWORK option!");
-    return 1;
   }
   return 0;
 }
 
 int main(const int argc, const char **argv) {
   if (argc < 5) {
-    cout << "usage: ./examples/xengine NETWORK "
-            "(lenet|vgg16|vgg19|resnet18|resnet34|resnet50|unet|googlenet) "
-            "BATCHSIZE "
-            "MODE (0=test|1=train) "
-            "OUTPUT_DIRECTORY (use '.' for default directory) "
-            "DEVICES (optional device file, default: "
-            "'../data/devices/devices_auto.txt')"
-         << endl;
-    return 1;
+    throw runtime_error(
+        "usage: ./examples/xengine NETWORK "
+        "(lenet|vgg16|vgg19|resnet18|resnet34|resnet50|unet|googlenet) "
+        "BATCHSIZE "
+        "MODE (0=test|1=train) "
+        "OUTPUT_DIRECTORY (use '.' for default directory) "
+        "DEVICES (optional device file, default: "
+        "'../data/devices/devices_auto.txt')");
   }
-  int batchsize = stoi(argv[2]);
+  const string name = argv[1];
+  const int batchsize = stoi(argv[2]);
   if (batchsize <= 0) {
     throw runtime_error("BATCHSIZE must be > 0!");
   }
-  int training = stoi(argv[3]);
+  const int training = stoi(argv[3]);
   if (training < 0 || training > 1) {
     throw runtime_error("MODE must be either 0 or 1! (0=test, 1=train)");
   }
@@ -153,6 +146,8 @@ int main(const int argc, const char **argv) {
   if (devices.find(".txt") == string::npos) {
     throw runtime_error("Device file must end with '.txt'");
   }
-  run(argv[1], batchsize, training, devices, output_dir);
-  return 1;
+
+  run(name, batchsize, training, devices, output_dir);
+
+  return 0;
 }
