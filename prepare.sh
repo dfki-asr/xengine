cc731d74481886170576d4a2dd14a920c4f8ebbb
Manuela Schuler <Manuela.Schuler@dfki.de>
2022-07-11 14:15:20 +0200

    EVALUATION: prepare.sh

---
+++ prepare.sh
@@ -0,0 +1,9 @@
+# usage: source [this file]
+# update these variable definitions to your library locations ...
+
+source /opt/intel/oneapi/setvars.sh --force
+
+export ONEAPI_HOME="$ONEAPI_ROOT"
+export ONEDNN_HOME="$HOME/Programs/oneDNN_2.4_Intel"
+export GUROBI_HOME="/opt/gurobi912/linux64"
+export CBC_HOME="$HOME/Repositories/CBC/dist"
